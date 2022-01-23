da78198393ca6955997bebf11ff8bed6365dc322
Hoel Bagard <hoel.bagard@gmail.com>
2022-01-23 19:28:55 +0900

    Made font size dynamic

--- src/render_ocr.py
+++ src/render_ocr.py
@@ -19,7 +19,6 @@ def render_detected(img: np.ndarray, ocr_data: OCRData, draw_bbox: bool = False)
     """
     img = Image.fromarray(img)
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("data/NotoSansJP-Regular.otf", 20)
 
     bboxes = list(zip(ocr_data["text"], ocr_data["left"], ocr_data["top"], ocr_data["width"], ocr_data["height"]))
     vertical_margin = 20  # Top / bottom margin when removing the background / text behind the bbox.
@@ -28,8 +27,17 @@ def render_detected(img: np.ndarray, ocr_data: OCRData, draw_bbox: bool = False)
     for _, left, top, width, height in bboxes:
         draw.rectangle((left, top-vertical_margin, left+width, top+height+vertical_margin), fill=255, width=0)
 
-    for char, left, top, width, height in bboxes:
-        draw.text((left, top), char, fill=1, font=font, direction="ttb", stroke_width=0)
+    for text, left, top, width, height in bboxes:
+        # Increase font size until it fills the bounding box
+        font_size = 7
+        font = ImageFont.truetype("data/NotoSansJP-Regular.otf", font_size)
+        text_font_width, text_font_height = font.getsize(text, direction="ttb")
+        while text_font_width < 0.90*width and text_font_height < 0.90*height:
+            font_size += 1
+            font = ImageFont.truetype("data/NotoSansJP-Regular.otf", font_size)
+            text_font_width, text_font_height = font.getsize(text, direction="ttb")
+
+        draw.text((left, top), text, fill=1, font=font, direction="ttb", stroke_width=0)
         if draw_bbox:
             draw.rectangle((left, top, left+width, top+height), fill=None, outline=None, width=1)
 
