964f4ec1fba71ee48516948aebe6bd634bc01843
Hoel Bagard <hoel.bagard@gmail.com>
2022-01-30 22:46:58 +0900

    Copy pasted more code.

--- src/clean_image.py
+++ src/clean_image.py
@@ -1,22 +1,115 @@
 import logging
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Optional
 
 import cv2
 import numpy as np
+import scipy.ndimage
 
-from src.utils.connected_components import get_cc_average_size
+from src.utils.connected_components import get_cc_average_size, get_connected_components, form_mask
 from src.utils.logger import create_logger
 from src.utils.misc import show_img
+
+
+def get_canny_hulls_mask(img: np.ndarray, mask: Optional[np.ndarray] = None) -> np.ndarray:
+    """Get the canny edges of the given image and draw blocks around them to form a mask.
+
+    Args:
+        img: The image to process.
+        mask: Optional, binary mask applied after the canny edges detection.
+
+    Returns:
+        The resulting mask
+    """
+    edges = cv2.Canny(img, 128, 255, apertureSize=3)
+    mask = mask*edges if mask is not None else edges
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    hulls_mask = np.zeros(img.shape, np.uint8)
+    for contour in contours:
+        hull = cv2.convexHull(contour)
+        cv2.drawContours(hulls_mask, [hull], 0, 255, -1)
+
+    return hulls_mask
+
+
+def cleaned2segmented(cleaned: np.ndarray, average_size: float, logger: logging.Logger):
+    vertical_smoothing_threshold = 0.75*average_size  # Note: the 0.75 used to be in the config
+    horizontal_smoothing_threshold = 0.75*average_size
+    height, width = cleaned.shape[:2]
+    logger.debug(f"Applying run length smoothing with vertical threshold {vertical_smoothing_threshold} "
+                 f"and horizontal threshold {horizontal_smoothing_threshold}")
+
+    run_length_smoothed = rls.RLSO(cv2.bitwise_not(cleaned), vertical_smoothing_threshold,
+                                   horizontal_smoothing_threshold)
+    components = get_connected_components(run_length_smoothed)
+    text = np.zeros((height, width), np.uint8)
+    for component in components:
+        seg_thresh = 1
+
+        def segment_into_lines(img, component, min_segment_threshold=1):
+            (ys, xs) = component[:2]
+            vertical = []
+            start_col = xs.start
+            for col in range(xs.start, xs.stop):
+                count = np.count_nonzero(img[ys.start:ys.stop, col])
+                if count <= min_segment_threshold or col == (xs.stop):
+                    if start_col >= 0:
+                        vertical.append((slice(ys.start, ys.stop), slice(start_col, col)))
+                        start_col = -1
+                elif start_col < 0:
+                    start_col = col
+
+        aspect, v_lines, h_lines = segment_into_lines(cv2.bitwise_not(cleaned), component,
+                                                      min_segment_threshold=seg_thresh)
+        if len(v_lines) < 2 and len(h_lines) < 2:
+            continue
+
+        # TODO: Wouldn't it be possible to just use the slice to set the values to 255 ?
+        cv2.rectangle(text, (component[1].start, component[0].start), (component[1].stop, component[0].stop), 255, -1)
+    return text
+
+
+# def filter_text_like_areas(img, segmentation, average_size):
+#   #see if a given rectangular area (2d slice) is very text like
+#   #First step is to estimate furigana like elements so they can be masked
+#   furigana_areas = furigana.estimate_furigana(img, segmentation)
+#   furigana_mask = np.array(furigana_areas==0,'B')
+
+#   #binarize the image, clean it via the segmentation and remove furigana too
+#   binary_threshold = arg.integer_value('binary_threshold',default_value=defaults.BINARY_THRESHOLD)
+#   if arg.boolean_value('verbose'):
+#     print('binarizing images with threshold value of ' + str(binary_threshold))
+#   binary = clean.binarize(img,threshold=binary_threshold)
+
+#   binary_average_size = cc.average_size(binary)
+#   if arg.boolean_value('verbose'):
+#     print('average cc size for binaryized grayscale image is ' + str(binary_average_size))
+#   segmentation_mask = np.array(segmentation!=0,'B')
+#   cleaned = binary * segmentation_mask * furigana_mask
+#   inv_cleaned = cv2.bitwise_not(cleaned)
+
+#   areas = cc.get_connected_components(segmentation)
+#   text_like_areas = []
+#   nontext_like_areas = []
+#   for area in areas:
+#     #if area_is_text_like(cleaned, area, average_size):
+#     if text_like_histogram(cleaned, area, average_size):
+#       text_like_areas.append(area)
+#     else:
+#       nontext_like_areas.append(area)
+
+#   return (text_like_areas, nontext_like_areas)
+
 
 
 def segment_image(img: np.ndarray,
                   logger: logging.Logger,
                   max_scale: float = 4.,
                   min_scale: float = 0.15):
-    h, w = img.shape[:2]
-
-    logger.info(f"Segmenting {h}x{w} image.")
+    height, width = img.shape[:2]
+    logger.info(f"Segmenting {height}x{width} image.")
 
     # Create gaussian filtered and unfiltered binary images
     binary_threshold = 190
@@ -25,67 +118,62 @@ def segment_image(img: np.ndarray,
 
     binary_average_size = get_cc_average_size(binary)
     logger.debug(f"Average cc size for binaryized grayscale image is {binary_average_size:.2f}")
-    exit()
 
     # The necessary sigma needed for Gaussian filtering (to remove screentones and other noise) seems
     # to be a function of the resolution the manga was scanned at (or original page size, I'm not sure).
-    # Assuming 'normal' page size for a phonebook style Manga is 17.5cmx11.5cm (6.8x4.5in).
+    # Assuming "normal" page size for a phonebook style Manga is 17.5cmx11.5cm (6.8x4.5in).
     # A scan of 300dpi will result in an image about 1900x1350, which requires a sigma of 1.5 to 1.8.
-    # I'm encountering many smaller images that may be nonstandard scanning dpi values or just smaller
-    # magazines. Haven't found hard info on this yet. They require sigma values of about 0.5 to 0.7.
+    # I'm encountering many smaller images that may be nonstandard scanning dpi values or just smaller magazines.
+    # Haven't found hard info on this yet. They require sigma values of about 0.5 to 0.7.
     # I'll therefore (for now) just calculate required (nonspecified) sigma as a linear function of vertical
     # image resolution.
-    sigma = (0.8/676.0)*float(h)-0.9
-    sigma = arg.float_value('sigma',default_value=sigma)
-    if arg.boolean_value('verbose'):
-        print('Applying Gaussian filter with sigma (std dev) of ' + str(sigma))
+    sigma = (0.8/676.0)*float(height)-0.9
+    logger.debug(f"Applying Gaussian filter with sigma (std dev) of {sigma}")
     gaussian_filtered = scipy.ndimage.gaussian_filter(img, sigma=sigma)
 
-    gaussian_binary = clean.binarize(gaussian_filtered,threshold=binary_threshold)
+    _, gaussian_binary = cv2.threshold(gaussian_filtered, binary_threshold, 255, cv2.THRESH_BINARY_INV)
 
-    #Draw out statistics on average connected component size in the rescaled, binary image
-    average_size = cc.average_size(gaussian_binary)
-    if arg.boolean_value('verbose'):
-        print('Binarized Gaussian filtered image average cc size: ' + str(average_size))
+    # Draw out statistics on average connected component size in the rescaled, binary image
+    average_size = get_cc_average_size(gaussian_binary)
+    logger.debug(f"Binarized Gaussian filtered image average cc size: {average_size}")
     max_size = average_size*max_scale
     min_size = average_size*min_scale
 
-    #primary mask is connected components filtered by size
-    mask = cc.form_mask(gaussian_binary, max_size, min_size)
+    # Create a mask based on the connected components's non zero values, filtered by size
+    mask = form_mask(gaussian_binary, max_size, min_size)
 
-    #secondary mask is formed from canny edges
-    canny_mask = clean.form_canny_mask(gaussian_filtered, mask=mask)
+    # Secondary mask is formed from the convex hulls around the canny edges (masked by the previous mask)
+    canny_mask = get_canny_hulls_mask(gaussian_filtered, mask=mask)
 
-    #final mask is size filtered connected components on canny mask
-    final_mask = cc.form_mask(canny_mask, max_size, min_size)
+    # Final mask is size filtered connected components on canny mask
+    final_mask = form_mask(canny_mask, max_size, min_size)
 
-    #apply mask and return images
+    # Apply mask and return images
     cleaned = cv2.bitwise_not(final_mask * binary)
     text_only = cleaned2segmented(cleaned, average_size)
 
-    #if desired, suppress furigana characters (which interfere with OCR)
-    suppress_furigana = arg.boolean_value('furigana')
-    if suppress_furigana:
-        if arg.boolean_value('verbose'):
-            print('Attempting to suppress furigana characters which interfere with OCR.')
-        furigana_mask = furigana.estimate_furigana(cleaned, text_only)
-        furigana_mask = np.array(furigana_mask==0,'B')
-        cleaned = cv2.bitwise_not(cleaned)*furigana_mask
-        cleaned = cv2.bitwise_not(cleaned)
-        text_only = cleaned2segmented(cleaned, average_size)
+    # If desired, suppress furigana characters (which interfere with OCR)
+    # suppress_furigana = arg.boolean_value('furigana')
+    # if suppress_furigana:
+    #     logger.debug("Attempting to suppress furigana characters which interfere with OCR.")
+    #     furigana_mask = furigana.estimate_furigana(cleaned, text_only)
+    #     furigana_mask = np.array(furigana_mask==0,'B')
+    #     cleaned = cv2.bitwise_not(cleaned)*furigana_mask
+    #     cleaned = cv2.bitwise_not(cleaned)
+    #     text_only = cleaned2segmented(cleaned, average_size)
 
-    (text_like_areas, nontext_like_areas) = filter_text_like_areas(img, segmentation=text_only, average_size=average_size)
-    if arg.boolean_value('verbose'):
-        print('**********there are ' + str(len(text_like_areas)) + ' text like areas total.')
+    (text_like_areas, nontext_like_areas) = filter_text_like_areas(img, segmentation=text_only,
+                                                                   average_size=average_size)
+    logger.debug(f"{len(text_like_areas)} potential textl areas have been detected in total.")
     text_only = np.zeros(img.shape)
     cc.draw_bounding_boxes(text_only, text_like_areas,color=(255),line_size=-1)
 
-    if arg.boolean_value('debug'):
-        text_only = 0.5*text_only + 0.5*img
-        #text_rows = 0.5*text_rows+0.5*gray
-        #text_colums = 0.5*text_columns+0.5*gray
+    # if arg.boolean_value('debug'):
+    #     text_only = 0.5*text_only + 0.5*img
+    #     # text_rows = 0.5*text_rows+0.5*gray
+    #     # text_colums = 0.5*text_columns+0.5*gray
 
-    segmented_image = np.zeros((h, w, 3), np.uint8)
+    segmented_image = np.zeros((height, width, 3), np.uint8)
     segmented_image[:, :, 0] = img
     segmented_image[:, :, 1] = text_only
     segmented_image[:, :, 2] = text_only
--- src/utils/connected_components.py
+++ src/utils/connected_components.py
@@ -10,14 +10,6 @@ DATE: Saturday, August 10th 2013
 import cv2
 import numpy as np
 import scipy.ndimage
-from pylab import zeros, amax, median
-
-
-def bbox_area(bbox: tuple[slice, slice]) -> int:
-    height = bbox[0].stop - bbox[0].start
-    width = bbox[1].stop - bbox[1].start
-    assert width > 0 and height > 0, f"Bounding box has {width=}, {height=}. They should be > 0"
-    return height * width
 
 
 def get_connected_components(img: np.ndarray) -> list[tuple[slice, slice]]:
@@ -39,6 +31,13 @@ def get_connected_components(img: np.ndarray) -> list[tuple[slice, slice]]:
     return objects_slices
 
 
+def bbox_area(bbox: tuple[slice, slice]) -> int:
+    height = bbox[0].stop - bbox[0].start
+    width = bbox[1].stop - bbox[1].start
+    assert width > 0 and height > 0, f"Bounding box has {width=}, {height=}. They should be > 0"
+    return height * width
+
+
 def get_cc_average_size(img: np.ndarray, minimum_area: int = 3, maximum_area: int = 100) -> float:
     """Compute the connected components of the given image, and return their average size after filtering by area.
 
@@ -53,12 +52,12 @@ def get_cc_average_size(img: np.ndarray, minimum_area: int = 3, maximum_area: in
     components = get_connected_components(img)
     sorted_components = sorted(components, key=bbox_area)
     # sorted_components = sorted(components,key=lambda x:area_nz(x,binary))
-    areas = zeros(img.shape)
+    areas = np.zeros(img.shape)
     for component in sorted_components:
         # If an area has already been set within the current component, then skip. (i.e. no overwriting)
         # Since the components are sorted from smallest to biggest, this means that we only keep the smaller ones.
         # (For example individual letters vs whole panel)
-        if amax(areas[component]) > 0:
+        if np.amax(areas[component]) > 0:
             continue
         # TODO: check if the area value used has a big influence.
         # Take the sqrt of the area of the bounding box
@@ -73,3 +72,28 @@ def get_cc_average_size(img: np.ndarray, minimum_area: int = 3, maximum_area: in
         return 0
     # Lastly take the median  # TODO: compare with average. Compare with median of set.
     return np.median(kept_areas)
+
+
+def form_mask(img: np.ndarray, max_size: float, min_size: float) -> np.ndarray:
+    """Create a binary (1 & 0s) mask based on the image and its connected components.
+
+    This keep only the elements of the image that belong to a connected components whose size is within
+    the given bounds.
+    TODO: This function also assume that the text is white on black (it is the case because of the inv binary threshold)
+
+    Args:
+        img (np.ndarray): The image to process.
+        max_size (float): Components bigger than this size are discarded (i.e. not included in the mask)
+        min_size (float): Same as above but with smaller components.
+
+    Returns:
+        The binary mask.
+    """
+    components = get_connected_components(img)
+    sorted_components = sorted(components, key=bbox_area)
+    mask = np.zeros(img.shape, np.uint8)
+    for component in sorted_components:
+        if bbox_area(component)**.5 < min_size or bbox_area(component)**.5 > max_size:
+            continue
+        mask[component] = img[component] > 0
+    return mask
